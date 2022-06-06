a235e483ceedb66a399a2a99f6e2fb754465c86e
Jordan <jordangopie@live.com>
2022-06-05 22:24:05 -0400

    Add DB to repo, create login html and script, enabled password hashing

--- Login.py
+++ Login.py
@@ -1,12 +1,13 @@
 from __main__ import app, login_manager
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, flash
 from users import User
-
+from werkzeug.security import check_password_hash
+from flask_login import login_user
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.get(int(user_id))
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -14,4 +15,11 @@ def login():
     if request.method == "POST":
         user_email = request.form["email"]
         user_password = request.form["password"]
+        user_info = User.query.filter_by(email=user_email).first()
+        check_pass = check_password_hash(user_info.password, user_password)
+        if check_pass:
+            login_user(user_info)
+            return render_template("secret.html")
+        else:
+            flash("Invalid Credentials")
     return render_template("login.html")
