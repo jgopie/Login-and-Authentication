928af7e2753b1adace1692d72a3ee61af0616daa
Jordan <jordangopie@live.com>
2022-06-05 23:53:42 -0400

    Fix Login and secret.html path

--- Registration.py
+++ Registration.py
@@ -10,8 +10,7 @@ def home():
         user_email = request.form["email"]
         user_password = request.form["password"]
         salted_password = generate_password_hash(password=user_password, method="pbkdf2:sha256", salt_length=8)
-        new_user = User(email=user_email, password=salted_password)
-        create_user(new_user)
+        create_user(email=user_email, password=salted_password)
         return redirect(url_for("login"))
     else:
         return render_template("index.html")
--- users.py
+++ users.py
@@ -1,10 +1,14 @@
 from __main__ import app
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+import random
+from datetime import datetime
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user_info.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+
 
 
 class User(UserMixin, db.Model):
@@ -12,8 +16,13 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), unique=True, nullable=False)
+    code = db.Column(db.String(6), unique=False, nullable=False)
+    creation_time = db.Column(db.String(20), unique=False, nullable=False)
 
-
-def create_user(user_to_add):
-    db.session.add(user_to_add)
+# db.create_all()
+def create_user(email, password):
+    code = "".join([str(random.randint(0, 9)) for _ in range(0, 6)])
+    time_of_creation = (datetime.now()).strftime("%d/%m/%Y %H:%M:%S")
+    new_user = User(email=email, password=password, code=code, creation_time=time_of_creation)
+    db.session.add(new_user)
     db.session.commit()
